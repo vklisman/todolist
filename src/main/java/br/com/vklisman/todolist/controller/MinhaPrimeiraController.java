05d46dbb115a27a2edccb12654ebf495771a62bb
Victor Klisman <vklisman@gmail.com>
2024-07-09 16:08:49 -0400

    feat: criando a primeira Controller

---
+++ src/main/java/br/com/vklisman/todolist/controller/MinhaPrimeiraController.java
@@ -0,0 +1,16 @@
+package br.com.vklisman.todolist.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/primeiraRota")
+public class MinhaPrimeiraController {
+
+
+    @GetMapping("/")
+    public String primeiraMensagem(){
+        return "Funcionou";
+    }
+}
