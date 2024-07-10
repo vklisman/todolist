a711172842eaa95290e19703ca1183a9e6b445a8
Victor Klisman <vklisman@gmail.com>
2024-07-10 00:33:56 -0400

    fix: criando tabela usuarios

--- src/main/java/br/com/vklisman/todolist/user/UserModel.java
+++ src/main/java/br/com/vklisman/todolist/user/UserModel.java
@@ -1,14 +1,26 @@
 package br.com.vklisman.todolist.user;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Data
+@Entity(name = "tb-users")
 public class UserModel {
 
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
     public String username;
-
     private String name;
-
     public String password;
 
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
