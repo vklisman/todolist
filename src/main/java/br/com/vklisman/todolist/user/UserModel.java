0f458bb40d0efa4b77ec0ab519419c30647cfd96
Victor Klisman <vklisman@gmail.com>
2024-07-09 21:04:46 -0400

    fix: adicionando getters e setters com lombok e database h2

--- src/main/java/br/com/vklisman/todolist/user/UserModel.java
+++ src/main/java/br/com/vklisman/todolist/user/UserModel.java
@@ -1,32 +1,14 @@
 package br.com.vklisman.todolist.user;
 
+import lombok.Data;
+
+@Data
 public class UserModel {
 
     public String username;
+
     private String name;
+
     public String password;
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
