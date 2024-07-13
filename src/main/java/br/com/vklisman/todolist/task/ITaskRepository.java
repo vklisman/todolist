dd2d81a6f12c662f5a6b3fd175a8df74bee04fb5
Victor Klisman <vklisman@gmail.com>
2024-07-13 00:03:55 -0400

    feat: listando as tarefas

--- src/main/java/br/com/vklisman/todolist/task/ITaskRepository.java
+++ src/main/java/br/com/vklisman/todolist/task/ITaskRepository.java
@@ -2,8 +2,10 @@ package br.com.vklisman.todolist.task;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
+    List<TaskModel> findByIdUser(UUID idUser);
 
 }
